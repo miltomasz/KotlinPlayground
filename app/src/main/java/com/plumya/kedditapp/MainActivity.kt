b312aec6a88a814a6d7ec3e9526e598eefa1f985
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2017-05-31 01:05:39 +0200

    Initial commit

---
+++ app/src/main/java/com/plumya/kedditapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.plumya.kedditapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+    }
+}
+
+}
+}
